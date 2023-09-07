a9ccf099580a6ecfc206d08f3bc2eb49c20d0a8f
comisiondlucas <140353133+comisiondlucas@users.noreply.github.com>
2023-09-07 15:39:42 -0300

    Add files via upload

---
+++ tarea 9/script.js
@@ -0,0 +1,28 @@
+// Definir dos variables
+var nombre = "Juan";
+var apellido = "Pérez";
+
+// Sumar las dos variables y mostrar el resultado en la consola
+console.log(nombre + " " + apellido);
+
+
+
+
+// Definir dos variables numéricas
+var numero1 = 10;
+var numero2 = 5;
+
+// Crear una tercera variable llamada "suma" que suma las dos primeras
+var suma = numero1 + numero2;
+
+// Mostrar la variable "suma" en la consola
+console.log("La suma de numero1 y numero2 es: " + suma);
+
+// Crear una variable numérica adicional
+var otroNumero = suma + 3;
+
+// Mostrar la variable "otroNumero" en la consola
+console.log("El resultado de sumar suma y 3 es: " + otroNumero);
+
+
+
